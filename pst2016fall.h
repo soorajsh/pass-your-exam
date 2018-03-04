2cc1698aec8f46d342c3c7e783bb737d42966783
willsir <36624796+willsir14@users.noreply.github.com>
2018-03-04 09:51:48 +0545

    few modifiations

--- pst2016fall.h
+++ pst2016fall.h
@@ -1,4 +1,3 @@
-
 void pst()
 {
     int year;
@@ -6,10 +5,11 @@ void pst()
     system("cls");
     printf("\nPlease enter the examination year: ");
     scanf("%d",&year);
-    printf("The year you have entered is %d",year);
+    printf("The year you have entered is %d\n",year);
     switch (year)
     {
         case 2016:
+        {
             printf("\nPress 'f' or 'F' for fall: ");
             printf("\nPress 's' or 'S' for spring: ");
             scanf(" %c",&fal_sp);
@@ -22,13 +22,41 @@ void pst()
                 break;
                 case 's':
                 case 'S':
-                    printf("\nSorry will be available soon");
+                    pst2016spring();
+                break;
+
+                default:
+                    printf("\nSorry not available\n");
+                break;
+            }
+        }
+        break;
+        case 2017:
+        {
+            printf("\nPress 'f' or 'F' for fall: ");
+            printf("\nPress 's' or 'S' for spring: ");
+            scanf(" %c",&fal_sp);
+            system("cls");
+            switch(fal_sp)
+            {
+                case 'f':
+                case 'F':
+                    pst2017fall();
+                break;
+                case 's':
+                case 'S':
+                   pst2017spring();
                 break;
 
                 default:
                     printf("\nSorry not available");
                 break;
             }
+        }
+        break;
+        default:
+            printf("\nSorry not available");
+        break;
     }
     return;
 }
@@ -36,7 +64,7 @@ void pst()
 void pst2016fall()
 {
     FILE *fpointer;
-    fpointer = fopen("pstfall2016.txt","r");
+    fpointer = fopen("pstfall2016f.txt","r");
 
     char qfall_2016[500];
 
@@ -44,6 +72,57 @@ void pst2016fall()
     {
         fgets(qfall_2016, 500, fpointer);
         printf(qfall_2016);
+
+    }
+    fclose(fpointer);
+    return;
+}
+
+void pst2016spring()
+{
+    FILE *fpointer;
+    fpointer = fopen("pstfall2016s.txt","r");
+
+    char qspring_2016[500];
+
+    while(!feof(fpointer))
+    {
+        fgets(qspring_2016, 500, fpointer);
+        printf(qspring_2016);
+
+    }
+    fclose(fpointer);
+    return;
+}
+
+void pst2017fall()
+{
+    FILE *fpointer;
+    fpointer = fopen("pstfall2017f.txt","r");
+
+    char qfall_2017[500];
+
+    while(!feof(fpointer))
+    {
+        fgets(qfall_2017, 500, fpointer);
+        printf(qfall_2017);
+
+    }
+    fclose(fpointer);
+    return;
+}
+
+void pst2017spring()
+{
+    FILE *fpointer;
+    fpointer = fopen("pstfall2017s.txt","r");
+
+    char qspring_2017[500];
+
+    while(!feof(fpointer))
+    {
+        fgets(qspring_2017, 500, fpointer);
+        printf(qspring_2017);
 
     }
     fclose(fpointer);
